0f7d343074d2b488d294725e105f1c6182085e59
james712346 <james712346@gmail.com>
2019-08-06 10:44:40 +1000

    Setting up the Game Engine

--- MasterMinds.py
+++ MasterMinds.py
@@ -1,11 +1,20 @@
-from tinydb import TinyDB, Query
+Colours = ["red", "orange", "yellow", "green","blue", "purple", "pink", "brown"]
+Objects = []
 
 class GameEngine():
     def __init__(self, GamePin):
         self.GamePin = GamePin
         self.WS = []
         self.req = query.GamePin == self.GamePin
-        self.liveInput = GameTB.get(req)["AvC"]
+        self.liveInput = GameTB.get(req)["AoC"] if GamePin else None
+        Objects.append(self)
+
+    def CreateGame(self, Length=4, NoC=4, Grade="3-4", UserID, Team):
+        self.GamePin = CreateUniqID(5)
+        self.req = query.GamePin == self.GamePin
+        self.liveInput = [Colours[i] for i in range(0,Length)]
+        GameTB.insert({"GamePin":self.GamePin, "Code": [random.choice(self.Colours) for i in range(0, Length)], "AvailableColours": NoC, "Grade":Grade, "Creation": time.time(), "Playing": False, "Team": Team})
+
 
     def play(self, UserID, ICode, Code=None):
         Code = Code if Code else GameTB.get(self.req)["Code"]
@@ -20,26 +29,35 @@ class GameEngine():
                 Code[i] = None
             if ICode[i] in Code and not done:
                 Code[Code.index(ICode[i])] = ICode[i].upper()
-        print(ICode,Code)
+        print(ICode, Code)
         return (UserID, ICode, Code.count(None), len([i for i in list(filter(None, Code)) if i.isupper()]))
+
     def Turns(self, UserID):
         if GameTB.get(req)["Gamemode"] == 'Solo':
             return TurnTB.search(self.req & query.UserID == UserID)
         else:
             return TurnTB.search(self.req)
 
+def GetObject(self, GamePin):
+    return Objects[[Object.GamePin for Object in Objects].index(GamePin)]
 
-
-
-
+def CreateUniqID(length):
+    id = ''.join([random.choice(string.ascii_letters + string.digits)
+                  for _ in range(1, length)])
+    while UserTB.search(query.UserID == id):
+        id = ''.join([random.choice(string.ascii_letters + string.digits)
+                      for n in range(1, length)])
+    return id
 
 
 if __name__ == "__main__":
-    query = Query()
+    from tinydb import TinyDB, Query
+    import time
     DB = TinyDB('Database.json')
     UserTB = DB.table("UserID")
     GameTB = DB.table("GamesPins")
     TurnTB = DB.table("Turns")
+    query = Query()
     GameEngine()
     while True:
         if
--- Webserver.py
+++ Webserver.py
@@ -9,47 +9,44 @@ import tornado.web
 from pyjade.ext.tornado import patch_tornado
 from tornado import template
 from tinydb import TinyDB, Query
-from masterminds import GameEngine as ge
+import MasterMinds
 patch_tornado()
+
 
 DB = TinyDB('Database.json')
 UserTB = DB.table("UserID")
 GameTB = DB.table("GamesPins")
 TurnTB = DB.table("Turns")
+query = Query()
 
-def CreateUniqID(length):
-    id = ''.join([random.choice(string.ascii_letters + string.digits)
-                  for _ in range(1, length)])
-    while UserTB.search(query.UserID == id):
-        id = ''.join([random.choice(string.ascii_letters +
-                                    string.digits) for n in range(1, length)])
-    return id
+
 
 class Home(tornado.web.RequestHandler):
     def get(self):
+        print("Join")
         if not self.get_cookie("id"):
-            self.set_cookie("id", CreateUniqID())
-            UserTB.insert({"UserID": self.get_cookie("id"), "LastLogin": time.time() "UserName": None, "Games": None, "Wins": None})
+            self.set_cookie("id", MasterMinds.CreateUniqID())
+            UserTB.insert({"UserID": self.get_cookie("id"), "LastLogin": time.time(), "UserName": None, "Games": None, "Wins": None})
         else:
-            UserTB.update({"LastLogin": time.time() + UserExpiry}, query.UserID == self.get_cookie("id"))
+            UserTB.update({"LastLogin": time.time() }, query.UserID == self.get_cookie("id"))
         self.render("Templates/Home.html")
 
 class JoinGame(tornado.web.RequestHandler):
     def get(self):
         if not self.get_cookie("id"):
-            self.set_cookie("id", CreateUniqID())
-            UserTB.insert({"UserID": self.get_cookie("id"), "LastLogin": time.time() "UserName": None, "Games": None, "Wins": None})
+            self.set_cookie("id", MasterMinds.CreateUniqID())
+            UserTB.insert({"UserID": self.get_cookie("id"), "LastLogin": time.time(), "UserName": None, "Games": None, "Wins": None})
         else:
-            UserTB.update({"LastLogin": time.time() + UserExpiry}, query.UserID == self.get_cookie("id"))
+            UserTB.update({"LastLogin": time.time() }, query.UserID == self.get_cookie("id"))
         self.render()
 
 class CreateGame(tornado.web.RequestHandler):
     def get(self):
         if not self.get_cookie("id"):
-            self.set_cookie("id", CreateUniqID())
-            UserTB.insert({"UserID": self.get_cookie("id"), "LastLogin": time.time() "UserName": None, "Games": None, "Wins": None})
+            self.set_cookie("id", MasterMinds.CreateUniqID())
+            UserTB.insert({"UserID": self.get_cookie("id"), "LastLogin": time.time(), "UserName": None, "Games": None, "Wins": None})
         else:
-            UserTB.update({"LastLogin": time.time() + UserExpiry}, query.UserID == self.get_cookie("id"))
+            UserTB.update({"LastLogin": time.time() }, query.UserID == self.get_cookie("id"))
         self.render()
 
 class Game(tornado.web.RequestHandler):
@@ -57,17 +54,33 @@ class Game(tornado.web.RequestHandler):
         if not GameTB.search(query.GamePin == GamePin):
             self.redirect("/Join?Pin=" + GamePin)
         if not self.get_cookie("id"):
-            self.set_cookie("id", CreateUniqID())
-            UserTB.insert({"UserID": self.get_cookie("id"), "LastLogin": time.time() "UserName": None, "Games": None, "Wins": None})
+            self.set_cookie("id", MasterMinds.CreateUniqID())
+            UserTB.insert({"UserID": self.get_cookie("id"), "LastLogin": time.time(), "UserName": None, "Games": None, "Wins": None})
             self.redirect("/Join") #Sends the query to back to the join page due to the query not have a id or username
         else:
-            UserTB.update({"LastLogin": time.time() + UserExpiry}, query.UserID == self.get_cookie("id"))
+            UserTB.update({"LastLogin": time.time() }, query.UserID == self.get_cookie("id"))
             if not UserTB.get(query.UserID == self.get_cookie("id"))["UserName"]:
                 self.redirect("/Join") #Send the query to back to the join page due to the query not have a username
             else:
-                if Game.get(query.ID == self.get_cookie("id"))["UserName"]
+                if Game.get(query.ID == self.get_cookie("id"))["UserName"]:
+                    pass
                 self.render("/Templates/Game.html")
 
+class MainWebsocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        pass
+    def on_message(self, message):
+        pass
+    def on_close(self):
+        pass
+
+class GameWebsocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        pass
+    def on_message(self, message):
+        pass
+    def on_close(self):
+        pass
 
 def Start():
     return tornado.web.Application([
@@ -89,3 +102,8 @@ def Start():
         (r'/Static/(.*)', tornado.web.StaticFileHandler, {'path': "Static/"})
 
     ], cookie_secret="qwrQzP8pyuykv7PGjReAmANxtLtAHz95")
+
+
+app = Start()
+app.listen(80)
+tornado.ioloop.IOLoop.current().start()
